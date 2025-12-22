58569ad24e14ccabbb02783217c84354355af4e3
MariaSelina <mariaselina2006@gmail.com>
2025-12-22 19:40:40 +0300

    Update test_car.py

--- src/test_car.py
+++ src/test_car.py
@@ -20,4 +20,4 @@ class TestCar(unittest.TestCase):
     
     def test_drive_insufficient(self):
         with self.assertRaises(Exception):
-            self.car.drive(100)
+            self.car.drive(100)# test CI
