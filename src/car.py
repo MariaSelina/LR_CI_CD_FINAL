e0bf3a95272af6903690f3ca058116aab786ad8f
MariaSelina <mariaselina2006@gmail.com>
2025-12-24 01:18:17 +0300

    Update car.py

--- src/car.py
+++ src/car.py
@@ -2,7 +2,7 @@ class Car:
     def __init__(self, model, fuel_capacity):
         self.model = model
         self.fuel_capacity = fuel_capacity
-        self.current_fuel = 0 
+        self.current_fuel = 0
     def refuel(self, amount):
         if self.current_fuel + amount > self.fuel_capacity:
             raise Exception("Бак переполнен")
